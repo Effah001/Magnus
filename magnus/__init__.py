9fb3aee77b9d091a63cdaf9bf93ebb6c1cc36164
Effah001 <effah210@gmail.com>
2024-05-02 05:16:06 +0000

    Added REgister, Login, update

--- magnus/__init__.py
+++ magnus/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 
 # Load environment variables from .env file
 load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dfkhksdfs35r49edfbjksfk32v3'
 
 # Configuration for MySQL database
 MYSQL_HOST = os.getenv('MYSQL_HOST')
@@ -20,5 +24,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@
 
 # Initialize SQLAlchemy
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+
+# Initialize Flask-Login's LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 from magnus import routes
--- magnus/models.py
+++ magnus/models.py
@@ -1,13 +1,16 @@
-from magnus import db
+from magnus import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
-
+    is_active =db.Column(db.Boolean(), default=True)
 
     def __repr__(self):
         """
@@ -16,3 +19,9 @@ class User(db.Model):
         user_string = "User({}, {}, {}, {}, {})"
         return user_string.format(self.id, self.username, self.password,
                                   self.email, self.created_at)
+
+
+# Define the user loader function
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
